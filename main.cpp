a4a085089d519bde112104ae31d7701e8a23355d
David2417pr <luis.figueroa29@upr.edu>
2024-11-29 19:56:10 +0000

    Pro??

--- main.cpp
+++ main.cpp
@@ -6,19 +6,20 @@ using namespace  std;
 const int COLS = 7;
 const int ROWS = 6;
 string Player1 = " X ", Player2 = " O ";
-
+bool winner = false, win1, win2;
 
 void DisplayBoard(string [][COLS],int ROWS);
-void Token(string [][COLS],int ROWS,string);
-void Token2(string Tok[][COLS],int ROWS, string);
-
+bool Token(string [][COLS],int ROWS,string, bool);
+bool Token2(string Tok[][COLS],int ROWS, string, bool);
+void Checkwinner1();
+void Checkwinner2();
 
 int main()
 {
 
-
-
 // Juego Board
+char opcion;
+bool CheckWinner, CheckWinner2, winpar1, winpar2;
 
 string Gamearray[ROWS][COLS] =    {{" - ", " - ", " - ", " - ", " - ", " - ", " - "}, 
                                 {" - ", " - ", " - ", " - ", " - ", " - ", " - "}, 
@@ -27,18 +28,36 @@ string Gamearray[ROWS][COLS] =    {{" - ", " - ", " - ", " - ", " - ", " - ", " 
                                 { " - ", " - ", " - ", " - ", " - ", " - ", " - "},
                                 { " - ", " - ", " - ", " - ", " - ", " - ", " - "}};
 
-// Secuencia de el juego
-int count = 0;
-while(count < 42){
-DisplayBoard(Gamearray, ROWS);
-Token(Gamearray, ROWS, Player1);
-DisplayBoard(Gamearray, ROWS);
-Token2(Gamearray, ROWS, Player2);
-count++;
-}
-
-
-return 0;
+do {
+ 
+   cout << "\n\t\tConnect Four!";
+   cout << "\n\t\tA.) Jugar.";
+   cout << "\n\t\tB.) Como jugar.";
+   cout << "\n\t\tC.) Salir";
+   cout << "\n\t\tOprima opcion: ";
+   cin >> opcion;
+   switch(opcion){
+    case 'a':
+    case 'A': // Secuencia de el juego
+                if(winner == true){
+                for(int i = 0; i < ROWS; i++)
+                    for(int j = 0; j < COLS; j++){
+                Gamearray[i][j] = " - ";
+                }
+                winner = false;}
+                while(CheckWinner == false || CheckWinner2 == false){
+                DisplayBoard(Gamearray, ROWS);
+                CheckWinner = Token(Gamearray, ROWS, Player1, winpar1);
+                DisplayBoard(Gamearray, ROWS);
+                CheckWinner = Token2(Gamearray, ROWS, Player2, winpar2);
+                }
+                break;
+    case 'b':
+    case 'B': cout << "Instrucciones de como jugar\n";
+                break;
+    case 'c':
+    case 'C': return 0;}
+}while(opcion != 'C' || opcion != 'c');
 
 }
 
@@ -54,14 +73,20 @@ for(int i = 0; i < ROWS; i++){
 }}
 // COls = 7, Rows = 6
 
-void Token(string Tok[][COLS],int ROWS, string Player){
+bool Token(string Tok[][COLS],int ROWS, string Player, bool WWinpar1){
 int Column, decrement = 1;
 bool lleno = true;
 //Input del Usuario y verificar si esta lleno
-
+win1 = false;
 cout << " Jugador " << Player;
-cout << "Entra columna (1-7): ";
+cout << "Entra columna (1-7)(-1): ";
 cin >> Column;
+
+if(Column == -1){
+    Checkwinner1();
+return win1;
+}
+
 while(Column < 1 || Column > 7){
     cout << "Las columnas son de 1 a 7: ";
     cin >> Column;    
@@ -97,19 +122,23 @@ while(Tok[ROWS-decrement][Column-1] == Player1 || Tok[ROWS-decrement][Column-1] 
     decrement++;    
 }
 Tok[ROWS-decrement][Column-1] = Player;
-
+return win1;
 }
 
 
 
-void Token2(string Tok[][COLS],int ROWS, string Player){
+bool Token2(string Tok[][COLS],int ROWS, string Player, bool WWinpar2){
 int Column, decrement = 1;
 bool lleno = true;
 //Input del Usuario y verificar si esta lleno
-
+win2 = false;
 cout << " Jugador " << Player;
-cout << "Entra columna (1-7): ";
+cout << "Entra columna (1-7)(-1): ";
 cin >> Column;
+if(Column == -1){
+    Checkwinner2();
+    return win2;
+}
 while(Column < 1 || Column > 7){
     cout << "Las columnas son de 1 a 7: ";
     cin >> Column;    
@@ -145,6 +174,13 @@ while(Tok[ROWS-decrement][Column-1] == Player1 || Tok[ROWS-decrement][Column-1] 
     decrement++;    
 }
 Tok[ROWS-decrement][Column-1] = Player;
+return win2;
 
 }
 
+void Checkwinner1(){
+    win1 = true;
+}
+void Checkwinner2(){
+    win2 = true;
+}
