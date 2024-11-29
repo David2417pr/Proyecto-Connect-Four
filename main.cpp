fc4dcb23cb245ead7304eb7c9d13aa644cb73734
David2417pr <luis.figueroa29@upr.edu>
2024-11-29 16:07:58 +0000

    Game1.0

--- main.cpp
+++ main.cpp
@@ -5,17 +5,20 @@ using namespace  std;
 
 const int COLS = 7;
 const int ROWS = 6;
-
+string Player1 = " X ", Player2 = " O ";
 
 
 void DisplayBoard(string [][COLS],int ROWS);
-void Token(string [][COLS],int ROWS);
-
+void Token(string [][COLS],int ROWS,string);
+void Token2(string Tok[][COLS],int ROWS, string);
 
 
 int main()
 {
 
+
+
+// Juego Board
 
 string Gamearray[ROWS][COLS] =    {{" - ", " - ", " - ", " - ", " - ", " - ", " - "}, 
                                 {" - ", " - ", " - ", " - ", " - ", " - ", " - "}, 
@@ -24,11 +27,15 @@ string Gamearray[ROWS][COLS] =    {{" - ", " - ", " - ", " - ", " - ", " - ", " 
                                 { " - ", " - ", " - ", " - ", " - ", " - ", " - "},
                                 { " - ", " - ", " - ", " - ", " - ", " - ", " - "}};
 
-
+// Secuencia de el juego
+int count = 0;
+while(count < 42){
 DisplayBoard(Gamearray, ROWS);
-Token(Gamearray, ROWS);
-
-
+Token(Gamearray, ROWS, Player1);
+DisplayBoard(Gamearray, ROWS);
+Token2(Gamearray, ROWS, Player2);
+count++;
+}
 
 
 return 0;
@@ -45,14 +52,42 @@ for(int i = 0; i < ROWS; i++){
         cout << Gamearr[i][j] << "|";
     }
 }}
+// COls = 7, Rows = 6
 
-void Token(string Tok[][COLS],int ROWS){
-int Column;
+void Token(string Tok[][COLS],int ROWS, string Player){
+int Column, decrement = 1;
+
+cout << "Jugador " << Player;
 cout << "Entra columna (1-7): ";
 cin >> Column;
 while(Column < 1 || Column > 7){
     cout << "Entra columna (1-7): ";
     cin >> Column;   
+}
+
+while(Tok[ROWS-decrement][Column-1] == Player1 || Tok[ROWS-decrement][Column-1] == Player2){
+    decrement++;    
+}
+Tok[ROWS-decrement][Column-1] = Player;
+
+
 
 }
+
+void Token2(string Tok[][COLS],int ROWS, string Player){
+int Column, decrement = 1;
+cout << " Jugador " << Player;
+cout << "Entra columna (1-7): ";
+cin >> Column;
+while(Column < 1 || Column > 7){
+    cout << "Entra columna (1-7): ";
+    cin >> Column;   
+} 
+while(Tok[ROWS-decrement][Column-1] == Player1 || Tok[ROWS-decrement][Column-1] == Player2){
+    decrement++;    
+}
+Tok[ROWS-decrement][Column-1] = Player;
+
+
+
 }
