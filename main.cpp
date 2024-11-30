3b8f475b43aa885b9f3f28e761e1508963be3aef
David2417pr <luis.figueroa29@upr.edu>
2024-11-30 17:22:13 +0000

    Progress1.0

--- main.cpp
+++ main.cpp
@@ -5,12 +5,12 @@ using namespace  std;
 
 const int COLS = 7;
 const int ROWS = 6;
-string Player1 = " X ", Player2 = " O ";
-bool winner = false;
+string Player1 = " X ", Player2 = " O ", Player;
+bool winner = false, Lleno2 = false;;
 
 void DisplayBoard(string [][COLS],int ROWS);
 void Token(string [][COLS],int ROWS,string);
-void Token2(string Tok[][COLS],int ROWS, string);
+void PlayerinTurn(string, string);
 
 
 
@@ -29,7 +29,7 @@ string Gamearray[ROWS][COLS] =    {{" - ", " - ", " - ", " - ", " - ", " - ", " 
 
 do {
  
-   cout << "\n\t\tConnect Four!";
+   cout << "\n\t\tConnect Four!\n";
    cout << "\n\t\tA.) Jugar.";
    cout << "\n\t\tB.) Como jugar.";
    cout << "\n\t\tC.) Salir";
@@ -44,15 +44,15 @@ do {
                 Gamearray[i][j] = " - ";
                 }
                 winner = false;}
-                while(winner == false){
+                while(winner == false && Lleno2 == false){
                 DisplayBoard(Gamearray, ROWS);
-                Token(Gamearray, ROWS, Player1);
-                DisplayBoard(Gamearray, ROWS);
-                Token2(Gamearray, ROWS, Player2);
+                PlayerinTurn(Player1, Player2);
+                Token(Gamearray, ROWS, Player);
+                
                 }
                 break;
     case 'b':
-    case 'B': cout << "Instrucciones de como jugar\n";
+    case 'B': cout << "\n\t\tEl primero que conecte la misma letra horizontalmente, verticalmente y/o diagonalmente, gana el juego.\n";
                 break;
     case 'c':
     case 'C': return 0;}
@@ -74,7 +74,7 @@ for(int i = 0; i < ROWS; i++){
 
 void Token(string Tok[][COLS],int ROWS, string Player){
 int Column, decrement = 1;
-bool lleno = true;
+bool Lleno = true;
 //Input del Usuario y verificar si esta lleno
 cout << " Jugador " << Player;
 cout << "Entra columna (1-7)(-1): ";
@@ -89,26 +89,45 @@ while(Column < 1 || Column > 7){
     cin >> Column;    
   
 }
-
-
 // Si da falso(Da falso es porque hay un espacio vacio), sale del ciclo while 
 
 for(int i = 1; i <= ROWS; i++){
     if (Tok[ROWS-i][Column-1]== Player1 || Tok[ROWS-i][Column-1]== Player2)
-    lleno = true;
+    Lleno = true;
+    
     else {
-    lleno = false;
+    Lleno = false;
     break;}
 }
 
-while(lleno == true){
-    cout << "La columna que has ingresado esta llena. Entre otra columna.";
+//Si no hay espacio en row 0, empate
+
+if(Lleno == true){
+    for(int j = 0; j < COLS; j++){
+            if(Tok[0][j] == Player1 || Tok[0][j] == Player2){
+            Lleno2 = true;
+            }
+            else {
+            Lleno2 = false;
+            break;}
+    }
+}
+if(Lleno2 == true){
+    cout << "\n\t\tEmpate!\n\n";
+    return;
+}
+
+// Si da falso(Da falso es porque hay un espacio vacio), sale del ciclo while 
+
+while(Lleno == true){
+    DisplayBoard(Tok, ROWS);
+    cout << "La columna que has ingresado esta llena. Jugador" << Player << " Entre otra columna.";
     cin >> Column;
     for(int i = 1; i <= ROWS; i++){
     if (Tok[ROWS-i][Column-1]== Player1 || Tok[ROWS-i][Column-1]== Player2)
-    lleno = true;
+    Lleno = true;
     else {
-    lleno = false;
+    Lleno = false;
     break;}
 }}
     
@@ -122,54 +141,13 @@ Tok[ROWS-decrement][Column-1] = Player;
 }
 
 
-
-void Token2(string Tok[][COLS],int ROWS, string Player){
-int Column, decrement = 1;
-bool lleno = true;
-//Input del Usuario y verificar si esta lleno
-cout << " Jugador " << Player;
-cout << "Entra columna (1-7)(-1): ";
-cin >> Column;
-
-if(Column == -1){
-    winner = true;
-    return;}
-
-while(Column < 1 || Column > 7){
-    cout << "Las columnas son de 1 a 7: ";
-    cin >> Column;    
-  
-}
-
-
-// Si da falso(Da falso es porque hay un espacio vacio), sale del ciclo while 
-
-for(int i = 1; i <= ROWS; i++){
-    if (Tok[ROWS-i][Column-1]== Player1 || Tok[ROWS-i][Column-1]== Player2)
-    lleno = true;
-    else {
-    lleno = false;
-    break;}
-}
-
-while(lleno == true){
-    cout << "La columna que has ingresado esta llena. Entre otra columna.";
-    cin >> Column;
-    for(int i = 1; i <= ROWS; i++){
-    if (Tok[ROWS-i][Column-1]== Player1 || Tok[ROWS-i][Column-1]== Player2)
-    lleno = true;
-    else {
-    lleno = false;
-    break;}
-}}
-    
-
-//Encontrar el espacio vacio
-
-while(Tok[ROWS-decrement][Column-1] == Player1 || Tok[ROWS-decrement][Column-1] == Player2){
-    decrement++;    
-}
-Tok[ROWS-decrement][Column-1] = Player;
+void PlayerinTurn(string Player1, string Player2){
+    static int number = 0;
+    if(number % 2 == 0)
+    Player = Player1;
+    else 
+    Player = Player2;
+    number++;
 
 }
 
