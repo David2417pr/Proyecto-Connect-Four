f028161a7f6b8fdf91f86a06b3af273912b20303
David2417pr <luis.figueroa29@upr.edu>
2024-12-05 01:56:52 +0000

    read2.0

--- README.txt
+++ README.txt
@@ -1,5 +1,10 @@
 
 		Nombre de los autores: Gabriel A. Sanchez Rivera (num. de estudiante: 801-23-8372), Luis D. Figueroa Olmo(num. de estudiante: 401-20-2128) y Aidan.
+
 		Seccion: 002
+
+		Colaboradores: Tuvimos que acudir a inteligencia artificial (chatgpt) para poder validar input cuando el usuario ingresaba una letra como ficha.
+
 		Dependencias requeridas para correr el juego : libreria limits(#include <limits>), string, iomanip.
-		Comentarios: Las fichas que se le asignaran a los jugadores son 'X' y 'O'
+
+		Comentarios: Las fichas que se le asignaran a los jugadores son 'X' y 'O'.
--- main.cpp
+++ main.cpp
@@ -7,9 +7,10 @@ int main() {
 
     ofstream outputBus;
     outputBus.open("README.txt");
-    outputBus << "\n\t\tNombre de los autores: Gabriel A. Sanchez Rivera (num. de estudiante: 801-23-8372), Luis D. Figueroa Olmo(num. de estudiante: 401-20-2128) y Aidan.\n";
-    outputBus << "\t\tSeccion: 002\n";
-    outputBus << "\t\tDependencias requeridas para correr el juego : libreria limits(#include <limits>), string, iomanip.\n";
-    outputBus << "\t\tComentarios: Las fichas que se le asignaran a los jugadores son 'X' y 'O'";
+    outputBus << "\n\t\tNombre de los autores: Gabriel A. Sanchez Rivera (num. de estudiante: 801-23-8372), Luis D. Figueroa Olmo(num. de estudiante: 401-20-2128) y Aidan.\n\n";
+    outputBus << "\t\tSeccion: 002\n\n";
+    outputBus << "\t\tColaboradores: Tuvimos que acudir a inteligencia artificial (chatgpt) para poder validar input cuando el usuario ingresaba una letra como ficha.\n\n";
+    outputBus << "\t\tDependencias requeridas para correr el juego : libreria limits(#include <limits>), string, iomanip.\n\n";
+    outputBus << "\t\tComentarios: Las fichas que se le asignaran a los jugadores son 'X' y 'O'.";
     outputBus.close();
 }
