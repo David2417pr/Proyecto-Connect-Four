852a9ceb5dd743b493e0af2e069ae0fbbc47271e
David2417pr <luis.figueroa29@upr.edu>
2024-12-12 19:53:03 +0000

    classes

--- main.cpp
+++ main.cpp
@@ -1,16 +1,85 @@
-#include <fstream>
+
 #include <iostream>
-#include <string>
+#include <iomanip>
+#include <string> 
 using namespace std;
 
-int main() {
+class Date
+{
+    private:
+    
+    int day, month, year;
+    string monthName() const;
 
-    ofstream outputBus;
-    outputBus.open("README.txt");
-    outputBus << "\n\t\tNombre de los autores: Gabriel A. Sanchez Rivera (num. de estudiante: 801-23-8372), Luis D. Figueroa Olmo(num. de estudiante: 401-20-2128) y Aidan.\n\n";
-    outputBus << "\t\tSeccion: 002\n\n";
-    outputBus << "\t\tColaboradores: Tuvimos que acudir a inteligencia artificial (chatgpt) para poder validar input cuando el usuario ingresaba una letra como ficha.\n\n";
-    outputBus << "\t\tDependencias requeridas para correr el juego : libreria limits(#include <limits>), string, iomanip.\n\n";
-    outputBus << "\t\tComentarios: Las fichas que se le asignaran a los jugadores son 'X' y 'O'.";
-    outputBus.close();
+    public:
+    
+    string getMMDDYYY() const;
+    string getMonthDayYear() const;
+    string getDayMonthYear() const;
+    Date();
+    ~Date();
+    void setDay(int);
+    void setMonth(int);
+    void setYear(int);
+};
+
+string Date:: getMMDDYYY() const;{
+    string date = to_string(month) + "/"
+                + "/"
+                + to_string(year);
+                return date;
+}
+
+string Date:: getMonthDayYear() const{
+string date = monthName() + " " 
+        + to_string(day) + ", "
+        + to_string(year);
+        return date;
+}
+
+string Date:: getDayMonthYear() const{
+string date = monthName() + " " 
+        + to_string(day) + ", "
+        + to_string(year);
+        return date;
+
+Date::Date(){
+    day = 1;
+    month = 1;
+    year = 1970;
+}
+~Date(){}
+
+void Date:: setDay(int d){
+    if( d >= 1 && d <= 31)
+    day = d;
+}
+void Date::setMonth(int m){
+    if( m >= 1 && . <= 12)
+    month = m;
+}
+void Date::setYear(int y){
+    if(y >= 1)
+    year = y;
+}
+
+string Date:: monthName() const{
+string monthNames[12] = {"January", "February", "March", "April", "May",
+                         "June", "July", "August", "September",
+                          "October", "November", "December"}
+                          return monthNames[month-1];
+
+}
+
+int main (){
+
+    Date today;
+    cout << today.getMMDDYYY() << endl;
+
+    today.setDay(12);
+    today.setMonth(12);
+    today.setYear(2024);
+
+    cout << today.getMonthDayYear;
+
 }
